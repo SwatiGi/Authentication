c1ea5890047113e62cbaa7bd6465a3fab7ddc8e6
Swati singh <swatigola01@gmail.com>
2025-09-20 15:23:23 +0530

    add

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,24 @@
-import {  Routes, Route } from 'react-router-dom';
+import {  Routes, Route ,Navigate} from 'react-router-dom';
 
 import Layout from './components/Layout/Layout';
 import UserProfile from './components/Profile/UserProfile';
 import AuthPage from './pages/AuthPage';
 import HomePage from './pages/HomePage';
+import { useContext } from 'react'
+import { AuthContext } from './store/AuthContext'
 
 
 function App() {
+  const authCtx = useContext(AuthContext);
   return (
     <Layout>
       <Routes>
         <Route path='/' element={<HomePage />} />
-        <Route path='/auth' element={<AuthPage />} />
-        <Route path='/profile' element={<UserProfile/>}/>
-      
+        {!authCtx.isLoggedIn&& <Route path='/auth' element={<AuthPage />} />}
+       
+        {authCtx.isLoggedIn&&<Route path='/profile' element={<UserProfile/>}/>}
+        
+        <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
       
     </Layout>
